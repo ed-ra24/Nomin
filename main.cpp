5fab554113fed73ebaddbc4d26aa8d117c3ea1b6
ed-ra24 <66018663+ed-ra24@users.noreply.github.com>
2020-05-27 09:04:35 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Elige una opcion\n");
+	printf("1. Nombre(s) y apellidos.\n");
+	printf("2. Categoría Ocupacional\n");
+	printf("3. Tasa Salarial\n");
+	printf("4. Horas Trabajadas.\n");
+	printf("5. Total de salarios trabajados\n");
+	printf("6. Total de salarios reubicados\n");
+	printf("7. Cumplimiento\n");
+	printf("8. Incumplimiento\n");
+	printf("9. Salario Total\n");
+	printf("10. Salir\n");
+	
+	int op;
+	scanf("%d",&op);
+	switch (op)
+	
+	{
+	case 1:
+		printf("\n");
+		scanf("");
+		break;
+	
+	case 2:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 3:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 4:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 5:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 6:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 7:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 8:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 9:
+		printf("\n");
+		scanf("");
+		break;
+		
+	case 10:
+		printf("\n");
+		scanf("");
+		break;
+	}
+	return 0;
+}
